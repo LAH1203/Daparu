63d45ff5eec1e2c863963909ab3282023e8a1382
배두리 <78518155+2ralumin@users.noreply.github.com>
2021-09-13 13:26:27 +0900

    db변경

--- Daparu/server/key/dev.js
+++ Daparu/server/key/dev.js
@@ -1,3 +1,2 @@
 module.exports={
- mongoURI : 'mongodb+srv://solitude:qodnfl1204@boilerplate.5a1bw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'}
- //'mongodb+srv://User:1111@cluster0.nl9qx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'}
+ mongoURI : 'mongodb+srv://User:1111@cluster0.nl9qx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'}
